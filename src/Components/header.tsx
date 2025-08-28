0667dbe7ea9f1659c56f4f2512518c38a1181653
shlomi noyshtat <s0583244814@gmail.com>
2025-08-28 21:03:26 +0300

    fix bug

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,10 +1,10 @@
-export default function Header(props:{changeStatus:string,setstatus:Function}){
+export default function Header(props:{changeStatus:boolean,setstatus:Function}){
     return(
         <>
         <h1>Campus Club Dashboard</h1>
         <main>
             <input type="checkbox" id='check' onClick={()=>{
-                props.setstatus(n => !n)
+                props.setstatus((n:boolean) => !n)
                 console.log(props.changeStatus)
             }}/>
             <label htmlFor="check">Show only active members</label>
